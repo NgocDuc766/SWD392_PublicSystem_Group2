2d1a1c289c00fbce67685f8b82bbf4e3b5120e04
ducbnhe173558 <ducbnhe173558@fpt.edu.vn>
2024-10-31 15:40:05 +0700

    add SubmitApplication and ListApplication

---
+++ Controllers/ResidentController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SWD392_PublicService.Controllers
+{
+    public class ResidentController : Controller
+    {
+        public IActionResult ListApplication()
+        {
+            return View();
+        }
+
+
+        public IActionResult SubmitApplication()
+        {
+            return View();
+        }
+    }
+}
